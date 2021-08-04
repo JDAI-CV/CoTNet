8712bb4ec472a0bb87bcfe6693db3602a815c0be
liyehao <yehaoli.sysu@gmail.com>
2021-08-04 16:11:39 +0800

    train.sh

--- cot_experiments/SE-CoTNetD-152_350epoch/train.sh
+++ cot_experiments/SE-CoTNetD-152_350epoch/train.sh
@@ -1,1 +1,9 @@
 python3 -m torch.distributed.launch --nproc_per_node=8 train.py --folder ./cot_experiments/SE-CoTNetD-152_350epoch
+
+# CUDA_VISIBLE_DEVICES=<GPU_0, GPU_1,...> NCCL_SOCKET_IFNAME=eth0 NCCL_NSOCKS_PERTHREAD=8 NCCL_SOCKET_NTHREADS=8 python3 -m torch.distributed.launch \
+#    --nproc_per_node=<#GPU> \
+#    --nnodes=<#node> \
+#    --node_rank=0 \
+#    --master_addr="<master node ip>" \
+#    --master_port=12346 \
+#    train.py --folder ./cot_experiments/SE-CoTNetD-152_350epoch
